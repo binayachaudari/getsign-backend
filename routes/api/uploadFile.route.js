28ad5633bdf3ac275f6900b6ba4fc03a39821503
binayachaudari <binayachaudari@live.com>
2023-01-16 13:33:57 +0545

    updated file routings

--- routes/api/uploadFile.route.js
+++ routes/api/uploadFile.route.js
@@ -2,6 +2,6 @@ const router = require('express').Router();
 const controller = require('../../controller/uploadFile.controller');
 
 router.post('/upload-file', controller.uploadFile);
-router.post('/get-file', controller.getFile);
+router.get('/get-file/:id', controller.getFile);
 
 module.exports = router;
