00952bbf548db8dbefcb75bc174e8d5703045816
binayachaudari <binayachaudari@live.com>
2023-02-15 11:25:53 +0545

    board get file according to itemId

--- services/board.js
+++ services/board.js
@@ -1,9 +1,19 @@
 const FileDetails = require('../models/FileDetails');
+const FileHistory = require('../models/FileHistory');
 const { isAlreadyAuthenticated } = require('./authenticatedBoard');
 
-const getStoredBoardFile = async (boardId) => {
+const getStoredBoardFile = async (boardId, itemId) => {
   try {
     const isAuthenticated = await isAlreadyAuthenticated(boardId);
+    const alreadySignedFile = await FileHistory.findOne({
+      item_id: itemId,
+      status: { $in: ['signed_by_receiver', 'signed_by_sender'] },
+    }).exec();
+
+    if (alreadySignedFile?.fileId) {
+      const doc = await FileDetails.findById(alreadySignedFile.fileId);
+      return { doc, isAuthenticated };
+    }
     const doc = await FileDetails.findOne({
       board_id: boardId,
       is_deleted: false,
