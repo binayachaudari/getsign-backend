95b2028a16ef07f8d2c75270afe4f27923a82376
binayachaudari <binayachaudari@live.com>
2023-03-09 15:56:01 +0545

    verify session token middleware created

---
+++ middleware/verifySessionToken.middleware.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+const { isUserAuthenticated } = require('../services/user.service');
+
+const verifySessionToken = async (req, res, next) => {
+  let decoded;
+  const token = req?.headers?.sessiontoken;
+
+  if (token) decoded = jwt.decode(token, process.env.CLIENT_SECRET);
+
+  const userId = decoded?.dat?.user_id;
+  const accountId = decoded?.dat?.account_id;
+  const user = await isUserAuthenticated(userId, accountId);
+
+  if (!user) {
+    next({ statusCode: 401, message: 'Unauthorized' });
+  }
+
+  req.isAuthenticated = true;
+  req.user = user;
+  next();
+};
+
+module.exports = { verifySessionToken };
