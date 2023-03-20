7241a2f7c9a9c204795e4b06379ff37bb5d2affa
binayachaudari <binayachaudari@live.com>
2023-03-20 15:52:26 +0545

    fix: email verification route `/email`

--- controller/email.controller.js
+++ controller/email.controller.js
@@ -1,14 +1,14 @@
 const { verifyEmailServie } = require('../services/email.service');
 
 const verifyEmail = async (req, res, next) => {
-  const { token } = req.params.token;
+  const { token } = req.params;
   try {
     if (!token) {
       return next({ statusCode: 400, message: 'Invalid verification token.' });
     }
-    const res = await verifyEmailServie(token);
+    const result = await verifyEmailServie(token);
 
-    return res.json({ data: res }).status(200);
+    return res.json({ data: result }).status(200);
   } catch (error) {
     return next({ statusCode: error?.status, message: error?.message });
   }
--- routes/api/index.js
+++ routes/api/index.js
@@ -4,5 +4,6 @@ router.use('/authorize', require('./authorization.route'));
 router.use('/files', require('./files.route'));
 router.use('/board', require('./board.route'));
 router.use('/monday', require('./monday.route'));
+router.use('/email', require('./email.route'));
 
 module.exports = router;
