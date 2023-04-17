7c7174c6b26146a120f460970c0ca2a400b78796
binayachaudari <binayachaudari@live.com>
2023-04-17 16:38:31 +0545

    update beforeInstall script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,5 +1,19 @@
 #!/bin/bash
 
+echo "Deplpyment Group is: $DEPLOYMENT_GROUP_NAME";
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDDev"]; then
+  cd /var/www//var/www/GetSign-Dev/jetsign-backend
+fi
+
+if [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDProd"]; then
+  cd /home/ubuntu/GetSign/jetsign-backend
+fi
+
+echo $SHELL
+
+sudo $SHELL
+
 echo "Node Version"
 sudo node -v
 
