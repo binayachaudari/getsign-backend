3da0253e9a5f172afddad56f4b5349671dd188cf
binayachaudari <binayachaudari@live.com>
2023-04-18 15:14:09 +0545

    CI/CD for master branch

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -3,13 +3,6 @@
 echo "Deplpyment Group is: $DEPLOYMENT_GROUP_NAME";
 
 sudo /bin/bash
-# sudo apt-get update
-# curl -fsSL https://deb.nodesource.com/setup_lts.x | sudo -E bash -
-# sudo apt-get install -y nodejs
-# sudo apt-get install npm  -y
-# sudo npm install -g yarn 
-
-# sudo yarn global add pm2
 
 echo "Node Version"
 sudo node -v
