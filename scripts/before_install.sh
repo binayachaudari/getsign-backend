6c5b2a7c871610e0cb02d844d72595fd24dc3c2f
binayachaudari <binayachaudari@live.com>
2023-05-18 12:45:54 +0545

    fix: before_install script

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -8,12 +8,12 @@ echo "Node Version"
 sudo node -v
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDDev" ]; then
-  DESTINATION_PATH="/var/www/GetSign-Dev/jetsign-backend"
+  DESTINATION_PATH="/var/www/GetSign-Dev/jetsign-backend/"
 fi
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDProd" ]; then
-  DESTINATION_PATH="/home/ubuntu/GetSign/jetsign-backend"
-  sudo cp -R /var/www/GetSign-Dev $DESTINATION_PATH
+  DESTINATION_PATH="/home/ubuntu/GetSign/jetsign-backend/"
+  sudo cp -R /var/www/GetSign-Dev/jetsign-backend/* $DESTINATION_PATH
 fi
 
 cd $DESTINATION_PATH
