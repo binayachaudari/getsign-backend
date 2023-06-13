23e69b94ceb32ae6fc43848a8087568d3ff7859f
binayachaudari <binayachaudari@live.com>
2023-06-13 16:21:16 +0545

    fix: webhook validator

--- validators/webhook.validator.js
+++ validators/webhook.validator.js
@@ -7,14 +7,14 @@ const appSubscriptionValidation = () => [
   body('data.account_id').trim().toInt(),
   body('data.user_email').trim().not().isEmpty().escape(),
   body('data.user_name').trim().not().isEmpty().escape(),
-  body('data.user_cluster').trim().not().isEmpty().escape(),
-  body('data.account_tier').trim().not().isEmpty().escape(),
+  body('data.user_cluster').trim().escape(),
+  body('data.account_tier').trim().escape(),
   body('data.account_name').trim().not().isEmpty().escape(),
   body('data.account_slug').trim().not().isEmpty().escape(),
   body('data.account_max_users').trim().not().isEmpty().toInt(),
   body('data.version_data').isObject(),
   body('data.timestamp').trim().not().isEmpty().escape(),
-  body('data.subscription').isObject(),
+  body('data.subscription').optional().isObject(),
 ];
 
 module.exports = {
