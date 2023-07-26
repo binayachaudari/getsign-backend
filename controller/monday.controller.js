3d2d878bd72b45f1945eceb5b5d02917a3473b05
Rasil07 <rasilgrt@gmail.com>
2023-07-26 17:40:00 +0545

    added route and services to creat new column

--- controller/monday.controller.js
+++ controller/monday.controller.js
@@ -1,6 +1,7 @@
 const {
   getItemDetails,
   getColumnValues,
+  runMondayQuery,
 } = require('../services/monday.service');
 
 const itemDetails = async (req, res, next) => {
@@ -23,7 +24,41 @@ const columnValues = async (req, res, next) => {
   }
 };
 
+const createNewColumn = async (req, res, next) => {
+  const { columnType, boardId, columnName } = req.params;
+
+  const query = `mutation createColumn($boardId: Int!, $columnType: ColumnType, $title: String!) {
+    create_column(board_id: $boardId, title: $title, column_type: $columnType) {
+      id
+      title
+      description
+    }
+  }`;
+
+  const options = {
+    variables: {
+      boardId: Number(boardId),
+      columnType: String(columnType),
+      title: String(columnName),
+    },
+  };
+
+  try {
+    await runMondayQuery({
+      query,
+      queryOptions: { ...options },
+      userId: req.userId,
+      accountId: req.accountId,
+    });
+
+    return res.status(200).json({ message: 'Created new column in board.' });
+  } catch (error) {
+    next(error);
+  }
+};
+
 module.exports = {
   itemDetails,
   columnValues,
+  createNewColumn,
 };
