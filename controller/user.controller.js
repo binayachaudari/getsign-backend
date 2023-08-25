33df59b4005ba5f00d876d781bdd0e03c079c484
Rasil07 <rasilgrt@gmail.com>
2023-08-25 13:01:38 +0545

    removed unwanted console

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -4,7 +4,6 @@ module.exports = {
   isEditorOnboarded: async (req, res, next) => {
     const { user } = req;
 
-    console.log({ user });
     return res
       .status(200)
       .json({ editorOnboarded: user?.editorOnboarded || false });
