c4dc6868afe71ba46bfb60ea543ce639f15ccf3a
binayachaudari <binayachaudari@live.com>
2023-08-25 16:37:03 -0400

    fix: email verification

--- services/email.service.js
+++ services/email.service.js
@@ -21,25 +21,25 @@ const verifyEmailServie = async token => {
       item.email_verification_token_expires = null;
       await item.save();
     }
+  } else {
+    const fileDetails = await FileDetails.findOne({
+      email_verification_token: token,
+      email_verification_token_expires: { $gt: Date.now() },
+    });
+
+    if (!fileDetails) {
+      throw {
+        status: 400,
+        message: 'Invalid or expired verification token.',
+      };
+    }
+
+    fileDetails.is_email_verified = true;
+    fileDetails.email_verification_token = null;
+    fileDetails.email_verification_token_expires = null;
+
+    await fileDetails.save();
   }
-
-  const fileDetails = await FileDetails.findOne({
-    email_verification_token: token,
-    email_verification_token_expires: { $gt: Date.now() },
-  });
-
-  if (!fileDetails) {
-    throw {
-      status: 400,
-      message: 'Invalid or expired verification token.',
-    };
-  }
-
-  fileDetails.is_email_verified = true;
-  fileDetails.email_verification_token = null;
-  fileDetails.email_verification_token_expires = null;
-
-  await fileDetails.save();
 
   return {
     message: 'Email verified successfully.',
