e840662c8560856208cd519db03efee46cea0186
binayachaudari <binayachaudari@live.com>
2023-09-27 01:53:06 -0400

    simple APIs for uploading and listing files

---
+++ routes/api/generate.route.js
@@ -0,0 +1,21 @@
+const router = require('express').Router();
+const generateController = require('../../controller/generate.controller');
+const {
+  verifySessionToken,
+} = require('../../middleware/verifySessionToken.middleware');
+
+router.post(
+  '/upload-document',
+  verifySessionToken,
+  generateController.uploadDocument
+);
+
+router.post('/templates', verifySessionToken, generateController.getTemplates);
+
+router.delete(
+  '/delete-file/:fileId',
+  verifySessionToken
+  // adhocController.deleteFile
+);
+
+module.exports = router;
---
+++ services/generate.service.js
@@ -0,0 +1,62 @@
+const ApplicationModel = require('../models/Application.model');
+const FileDetails = require('../models/FileDetails');
+const { backOfficeUploadedDocument } = require('./backoffice.service');
+
+const uploadDocumentToGeneratePDF = async req => {
+  const body = req.body;
+  const file = req.files.files;
+
+  // const fileDetails = await FileDetails.findOne({
+  //   account_id: body.account_id,
+  //   board_id: Number(body.board_id),
+  //   type: 'generate',
+  // }).sort({ created_at: -1 });
+
+  const s3Res = await s3
+    .upload({
+      Bucket: process.env.IS_DEV
+        ? `${process.env.BUCKET_NAME}/dev-test`
+        : process.env.BUCKET_NAME,
+      Key: `get-sign-${file.name}-${Date.now().toString()}`,
+      Body: file.data,
+      ContentType: file.mimetype,
+    })
+    .promise();
+
+  const result = await FileDetails.create({
+    account_id: body.account_id,
+    board_id: body.board_id,
+    item_id: body.item_id,
+    user_id: body.user_id,
+    file_name: file.name,
+    file: s3Res.Key,
+    itemViewInstanceId: body.instanceId,
+    type: 'generate',
+  });
+
+  await result.save();
+
+  const appInstallDetails = await ApplicationModel.findOne({
+    type: 'install',
+    account_id: body.account_id,
+  }).sort({ created_at: 'desc' });
+
+  if (appInstallDetails?.back_office_item_id) {
+    await backOfficeUploadedDocument(appInstallDetails.back_office_item_id);
+  }
+
+  return result;
+};
+
+const templates = async boardId => {
+  return FileDetails.find({
+    board_id: boardId,
+    is_deleted: false,
+    type: 'generate',
+  });
+};
+
+module.exports = {
+  uploadDocumentToGeneratePDF,
+  templates,
+};
