4fedfa137ce0a164dc1c85e11130f941d1337b0c
Rasil07 <rasilgrt@gmail.com>
2023-11-09 14:11:26 +0545

    added migration up

---
+++ migrations/add_assignedReciever_filehistory/up.js
@@ -0,0 +1,104 @@
+const { default: mongoose, Types } = require('mongoose');
+const fs = require('fs');
+const SignerModel = require('../../models/Signer.model');
+const FileHistory = require('../../models/FileHistory');
+
+const CONFIG = {
+  migrationLogTypes: {
+    error: 'error',
+    completed: 'completed',
+    failed: 'failed',
+  },
+};
+
+connectDB(); // Initialize DB
+
+const MIGRATION_LOG_FILE = 'add_assignedReciever_filehistory_logs.json';
+
+let successfulMigrationCount = 0;
+let unSuccessFullMigrationCount = 0;
+let documentProcessed = 0;
+
+let migrationEventLogStream;
+
+class LogManager {
+  constructor(type, signerDocument, msg) {
+    this.type = type;
+    this.signerDocument = signerDocument;
+    this.message = msg;
+  }
+
+  async writeLog() {
+    const data = {
+      type: this.type,
+      signerDocument: this.signerDocument,
+      message: this.message,
+    };
+    migrationEventLogStream.write(JSON.stringify(data) + ',' + '\n');
+  }
+}
+
+class MigrationHandler {
+  constructor(signerDetail) {
+    this.signerDetail = signerDetail;
+  }
+
+  async startTransaction() {}
+
+  async handleError() {}
+}
+
+async function startMigration() {
+  const isFileExists = fs.existsSync(MIGRATION_LOG_FILE);
+  if (isFileExists) {
+    fs.truncateSync(MIGRATION_LOG_FILE, 0, err => {
+      if (err) {
+        console.log('Could not truncate the event file');
+      } else {
+        console.log('Successfully truncate the event file');
+      }
+    });
+  }
+  migrationEventLogStream = fs.createWriteStream(MIGRATION_LOG_FILE, {
+    flags: 'a',
+  });
+  migrationEventLogStream.write('[\n');
+
+  const signerDetailCursor = SignerModel.find({}).batchSize(500).cursor();
+
+  for await (doc of signerDetailCursor) {
+    documentProcessed++;
+    console.log(' Processing document number ==>', documentProcessed, '\n');
+    const migrationHandler = new MigrationHandler(doc);
+    try {
+      await migrationHandler.startTransaction();
+    } catch (err) {
+      await migrationHandler.handleError(err);
+    }
+  }
+
+  migrationEventLogStream.write(
+    JSON.stringify({ documentProcessed }) + ',' + '\n'
+  );
+  migrationEventLogStream.write(
+    JSON.stringify({ successfulMigrationCount }) + ',' + '\n'
+  );
+  migrationEventLogStream.write(
+    JSON.stringify({ unSuccessFullMigrationCount }) + '\n'
+  );
+  migrationEventLogStream.write('\n]', err => {
+    if (err) {
+      console.log('Error while closing array');
+    } else {
+      console.log('Success at closing array');
+    }
+    process.exit(0);
+  });
+
+  // await fileDetailCursor
+  //   .eachAsync(async function (doc) {})
+  //   .catch(err => console.log('Error while running the curson', err))
+  //   .finally(() => {});
+}
+
+startMigration();
