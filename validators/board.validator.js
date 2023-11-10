8a6671ad666d5bc0431261d2aea472dc0e91d5ef
Rasil07 <rasilgrt@gmail.com>
2023-11-10 15:15:11 +0545

    added request validator

--- validators/board.validator.js
+++ validators/board.validator.js
@@ -1,10 +1,18 @@
-const { param } = require('express-validator');
+const { param, body } = require('express-validator');
 
 const boardGetBoardFileValidator = () => [
   param('boardId').trim().not().isEmpty().toInt(),
   param('itemId').trim().not().isEmpty().toInt(),
 ];
 
+const addColumnToBoardValidator = () => [
+  param('board').trim().not().isEmpty().toInt(),
+  body('title').trim().not().isEmpty().escape(),
+  body('description').trim().optional().escape(),
+  body('column_type').trim().not().isEmpty().escape(),
+];
+
 module.exports = {
   boardGetBoardFileValidator,
+  addColumnToBoardValidator,
 };
