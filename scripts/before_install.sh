c7cf946bca245634b05705c7cd4310c1e4495e59
binayachaudari <binayachaudari@live.com>
2023-11-21 14:36:11 -0500

    try to fix pipeline

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -2,22 +2,18 @@
 
 echo "Deplpyment Group is: $DEPLOYMENT_GROUP_NAME";
 
-sudo /bin/bash
-
 echo "Node Version"
 sudo node -v
 
 if [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDDev" ]; then
-  DESTINATION_PATH="/var/www/GetSign-Dev/jetsign-backend/"
+  DESTINATION_PATH="/var/www/GetSign-Dev/jetsign-backend"
 elif [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDQA" ]; then
-  DESTINATION_PATH="/var/www/GetSign-QA/jetsign-backend/"
+  DESTINATION_PATH="/var/www/GetSign-QA/jetsign-backend"
 elif [ "$DEPLOYMENT_GROUP_NAME" == "GetSignBackEndCICDProd" ]; then
-  DESTINATION_PATH="/home/ubuntu/GetSign/jetsign-backend/"
+  DESTINATION_PATH="/home/ubuntu/GetSign/jetsign-backend"
 else 
   echo "Unsupported environment"
   exit 1
 fi
 
-sudo \cp -R /var/www/GetSign-Temp/jetsign-backend/* $DESTINATION_PATH
-cd $DESTINATION_PATH
-sudo yarn --frozen-lockfile
+sudo cp -R /var/www/GetSign-Temp/jetsign-backend $DESTINATION_PATH
